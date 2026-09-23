9795ca03e52eacbf8587433a58532993779130f8
Dinmukhamed <dinmukhamed.b@halykmarket.com>
2026-09-23 11:44:38 +0500

    fix: load site without Telegram SDK

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(VueTelegramPlugin)
+if ('Telegram' in window) app.use(VueTelegramPlugin)
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 app.mount('#app')
